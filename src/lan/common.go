0f9d14a92f3319e5953b8c19a103d306101c3cfb
lylf <1014661165@qq.com>
2020-08-17 13:24:15 +0800

    update

--- src/lan/common.go
+++ src/lan/common.go
@@ -1,7 +1,10 @@
 package lan
 
-import "../util"
-import "strings"
+import (
+	"strings"
+
+	"../util"
+)
 
 const LANGUAGE_C = "c"
 const C_INCLUDE = "include"
@@ -19,17 +22,16 @@ const JAVA_ACCESS_DEFAULT = "default"
 const JAVA_EXTENDS = "extends"
 const JAVA_IMPLEMENTS = "implements"
 const JAVA_THROWS = "throws"
-const JAVA_KEYWORDS_WITH_PAREN = "for,switch,catch,if,while"
+const JAVA_KEYWORDS_WITH_PAREN = "for,switch,catch,if,while,try,synchronized"
 
 const LANGUAGE_CPP = "cpp"
 const CPP_CLASS = "class"
 const CPP_NAMESPACE = "namespace"
 const CPP_ACCESS = "public,private,protected"
 
-
 type File struct {
 	Path string `json:"path"`
-	Line int `json:"line"`
+	Line int    `json:"line"`
 }
 
 type IFile interface {
@@ -38,35 +40,35 @@ type IFile interface {
 }
 
 //处理注释
-func ProcessComment1(content []byte, idx *int, size int, line *int){
+func ProcessComment1(content []byte, idx *int, size int, line *int) {
 	var char string
 	for {
 		*idx++
-		if *idx >= size{
+		if *idx >= size {
 			break
 		}
 		char = string(content[*idx])
 		CheckLine(char, line)
-		if char == "\n"{
+		if char == "\n" {
 			break
 		}
 	}
 }
-func ProcessComment2(content []byte, idx *int, size int, line *int){
+func ProcessComment2(content []byte, idx *int, size int, line *int) {
 	var char string
 	for {
 		*idx++
-		if *idx >= size{
+		if *idx >= size {
 			break
 		}
 		char = string(content[*idx])
 		CheckLine(char, line)
-		if char == "*"{
-			if *idx+1 >= size{
+		if char == "*" {
+			if *idx+1 >= size {
 				break
 			}
 			char = string(content[*idx+1])
-			if char == "/"{
+			if char == "/" {
 				*idx++
 				break
 			}
@@ -75,59 +77,58 @@ func ProcessComment2(content []byte, idx *int, size int, line *int){
 }
 
 //判断是否需要增加行数
-func CheckLine(char string, line *int){
-	if char == "\n"{
+func CheckLine(char string, line *int) {
+	if char == "\n" {
 		*line++
 	}
 }
 
 //处理字符串变量
-func ProcessString(content []byte, idx *int, size int){
+func ProcessString(content []byte, idx *int, size int) {
 	char := string(content[*idx])
 	for {
 		*idx++
-		if *idx >= size{
+		if *idx >= size {
 			break
 		}
-		if content[*idx] == '\\'{
+		if content[*idx] == '\\' {
 			*idx++
 			continue
 		}
 		char = string(content[*idx])
-		if char == "\""{
+		if char == "\"" {
 			break
 		}
 	}
 }
 
-
 //获取前面n个标识符
-func GetFrontWords(content []byte, idx int, n int) []string{
+func GetFrontWords(content []byte, idx int, n int) []string {
 	tmpIndex := idx
 	var char string
 	//跳过n个标识符
 	s := make([]string, n)
-	for i:=0;i<n; i++ {
+	for i := 0; i < n; i++ {
 		for {
 			tmpIndex--
-			if tmpIndex < 0{
+			if tmpIndex < 0 {
 				break
 			}
 			char = string(content[tmpIndex])
-			if !util.IsSpace(char){
+			if !util.IsSpace(char) {
 				break
 			}
 		}
 		tmpS := make([]string, 0)
 		for {
-			if util.IsSpace(char){
+			if util.IsSpace(char) {
 				break
 			}
-			if char != "*"{
+			if char != "*" {
 				tmpS = append(tmpS, char)
 			}
 			tmpIndex--
-			if tmpIndex < 0{
+			if tmpIndex < 0 {
 				break
 			}
 			char = string(content[tmpIndex])
@@ -137,50 +138,49 @@ func GetFrontWords(content []byte, idx int, n int) []string{
 	return s
 }
 
-
 //从方法体中查找api调用
-func FindApis(chars []byte, filterKeywords string) []string{
+func FindApis(chars []byte, filterKeywords string) []string {
 	var char string
 	index := 0
 	apis := make([]string, 0)
 	for {
-		if index >= len(chars){
+		if index >= len(chars) {
 			break
 		}
 		char = string(chars[index])
-		if char == "\""{
+		if char == "\"" {
 			ProcessString(chars, &index, len(chars))
 		}
 
-		if char == "("{
+		if char == "(" {
 			tmpIndex := index
 			var tmpChar string
 			apiName := make([]string, 0)
 			for {
 				tmpIndex--
-				if tmpIndex < 0{
+				if tmpIndex < 0 {
 					break
 				}
 				tmpChar = string(chars[tmpIndex])
-				if !util.IsSpace(tmpChar){
+				if !util.IsSpace(tmpChar) {
 					break
 				}
 			}
 			for {
-				if !util.IsIdentifier(tmpChar){
+				if !util.IsIdentifier(tmpChar) {
 					break
 				}
 				apiName = append([]string{tmpChar}, apiName...)
 				tmpIndex--
-				if tmpIndex < 0{
+				if tmpIndex < 0 {
 					break
 				}
 				tmpChar = string(chars[tmpIndex])
 			}
-			if len(apiName) != 0{
+			if len(apiName) != 0 {
 				api := strings.Join(apiName, "")
-				if api != ""{
-					if !strings.Contains(filterKeywords, api){
+				if api != "" {
+					if !strings.Contains(filterKeywords, api) {
 						apis = append(apis, api)
 					}
 				}
@@ -192,32 +192,32 @@ func FindApis(chars []byte, filterKeywords string) []string{
 }
 
 //判断是否为目标词
-func IsTargetWord(content []byte, idx int, target string) bool{
+func IsTargetWord(content []byte, idx int, target string) bool {
 	char := string(content[idx])
 	tmpIndex := idx
 	var s string
 	for {
 		s += char
 		tmpIndex++
-		if tmpIndex >= len(content){
+		if tmpIndex >= len(content) {
 			break
 		}
 		char = string(content[tmpIndex])
-		if !util.IsIdentifier(char){
+		if !util.IsIdentifier(char) {
 			break
 		}
 	}
 	preCharIsSpace := true
 	nextCharisSpace := true
 
-	if !util.IsSpace(char){
+	if !util.IsSpace(char) {
 		nextCharisSpace = false
 	}
-	if idx - 1 >= 0{
-		char = string(content[idx - 1])
-		if !util.IsSpace(char){
+	if idx-1 >= 0 {
+		char = string(content[idx-1])
+		if !util.IsSpace(char) {
 			preCharIsSpace = false
 		}
 	}
-	return  s == target && preCharIsSpace && nextCharisSpace
+	return s == target && preCharIsSpace && nextCharisSpace
 }
